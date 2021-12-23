b7c618cae393a30835ef2aeab6219c752b3baa97
Jack Liao <90374371+ShunKetsuRyou@users.noreply.github.com>
2021-12-23 11:28:48 +0800

    Create  AuthorityInformation.kt

---
+++ app/src/main/java/com/alien/efalnventory/dataModel/localDataProcessModel/AuthorityInformation.kt
@@ -0,0 +1,9 @@
+package com.alien.efaInventory.dataModel.localDataProcessModel
+
+class AuthorityInformation {
+    var id: String = ""
+    var deptName: String = ""
+    var deptEnName: String = ""
+    var count: Int = 0
+    var isSelected: Boolean = false
+}
