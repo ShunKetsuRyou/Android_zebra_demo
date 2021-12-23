dab773b3f50513e102940a0876b39ca6cb503a74
Jack Liao <90374371+ShunKetsuRyou@users.noreply.github.com>
2021-12-23 11:53:13 +0800

    Create  ProgressionBar.kt

---
+++ app/src/main/java/com/alien/efalnventory/util/ProgressionBar.kt
@@ -0,0 +1,32 @@
+package com.alien.efaInventory.util
+
+import android.app.Activity
+import android.app.AlertDialog
+
+import com.alien.efaInventory.R
+
+
+class ProgressionBar(private var activity: Activity? = null){
+
+
+    private var alertDialog2: AlertDialog? = null
+
+    fun loading (myActivity: Activity) {
+        activity = myActivity
+    }
+    fun startLoading() {
+        val builder = AlertDialog.Builder(activity)
+        val inflater = activity!!.layoutInflater
+        builder.setView(inflater.inflate(R.layout.progression_layout, null))
+        //等於true時滑鼠點一下即可暫停
+        //等於false時則不會暫停
+        builder.setCancelable(true)
+        alertDialog2 = builder.create()
+        alertDialog2!!.show()
+    }
+
+    fun dismissLoading() {
+        alertDialog2!!.dismiss()
+    }
+
+}
