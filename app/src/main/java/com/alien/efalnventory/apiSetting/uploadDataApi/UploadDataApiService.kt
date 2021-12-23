6363166e1d0b2da65b4f922b1df3b361c27cf6bb
Jack Liao <90374371+ShunKetsuRyou@users.noreply.github.com>
2021-12-23 10:25:26 +0800

    Create  UploadDataApiService.kt

---
+++ app/src/main/java/com/alien/efalnventory/apiSetting/uploadDataApi/UploadDataApiService.kt
@@ -0,0 +1,33 @@
+package com.alien.efaInventory.apiSetting.uploadDataApi
+
+import kotlinx.serialization.json.Json
+import retrofit2.Retrofit
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.ExperimentalSerializationApi
+import okhttp3.MediaType.Companion.toMediaType
+
+
+class UploadDataApiService {
+    @ExperimentalSerializationApi
+    fun  uploadDataRetrofitManager(): Retrofit.Builder {
+
+        val retrofit = Retrofit.Builder()
+        val contentType = "application/json".toMediaType()
+
+        return retrofit
+            .baseUrl("http://ptwsvcdev.pegatroncorp.com")
+            .addConverterFactory(Json.asConverterFactory(contentType))
+    }
+
+    @ExperimentalSerializationApi
+    fun uploadDataAuthorityClient(): Retrofit {
+
+        return uploadDataRetrofitManager().build()
+    }
+
+
+
+
+
+
+}
