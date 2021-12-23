16db0bbe96ba78fcfcb58f2f58a28125d105d6e0
Jack Liao <90374371+ShunKetsuRyou@users.noreply.github.com>
2021-12-23 11:25:00 +0800

    Create  DownloadDataApiCallback.kt

---
+++ app/src/main/java/com/alien/efalnventory/dataModel/downloadDataApiModel/DownloadDataApiCallback.kt
@@ -0,0 +1,13 @@
+package com.alien.efaInventory.dataModel.downloadDataApiModel
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class DownloadDataApiCallback(
+    @SerialName("respCode") val respCode: String?,
+    @SerialName("respMsg") val respMsg: String?,
+    @SerialName("respData") val respData: String?,
+    @SerialName("fail") val fail: Boolean?
+
+)
